2746232b54d601330c34c9ce55421f66319cd0b8
Nick Timaskovs <101591475+nicktmv@users.noreply.github.com>
2024-04-07 17:42:14 +0100

    better visualisation with images, file renames

--- four_d_radar/utils/paths.py
+++ four_d_radar/utils/paths.py
@@ -5,3 +5,6 @@ class Paths:
         self.train_indices_path = '../../data/view_of_delft_PUBLIC/lidar/ImageSets/train.txt'
         self.test_indices_path = '../../data/view_of_delft_PUBLIC/lidar/ImageSets/test.txt'
         self.pose_path = '../../data/view_of_delft_PUBLIC/lidar/training/pose'
+        self.image_path = '../../data/view_of_delft_PUBLIC/lidar/training/image_2'
+        self.calib_path = '../../data/view_of_delft_PUBLIC/lidar/training/calib'
+
