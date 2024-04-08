0d4d249c832b022222798723812cebbca58adf35
Nick Timaskovs <101591475+nicktmv@users.noreply.github.com>
2024-04-08 21:27:38 +0100

    allowing main to print the path

--- four_d_radar/main.py
+++ four_d_radar/main.py
@@ -1,10 +1,14 @@
-from four_d_radar.utils.data_paths import DataPaths  # Ensure you import DataPaths correctly from wherever it's defined
+from four_d_radar.utils.data_paths import DataPaths
 
 
 def main():
     root_dir = "D:/four-d-radar-thesis"
+
+    # Initialize the DataPaths class with the root directory
     paths = DataPaths(root_dir)
-    print(paths.data_path)
+
+    # Example usage: Print the absolute path to the point cloud files
+    print(paths.pointcloud_file_path)
 
 
 if __name__ == "__main__":
--- four_d_radar/utils/data_paths.py
+++ four_d_radar/utils/data_paths.py
@@ -1,44 +1,41 @@
-"""
-A class for storing the paths to the data used in the project.
-"""
 from pathlib import Path
 
 
 class DataPaths:
-    def _init_(self, root_dir: str):
+    def __init__(self, root_dir: str):
         self.__root_dir = Path(root_dir)
-        self.pointcloud_file_path = 'data/view_of_delft_PUBLIC/radar/training/velodyne'
-        self.label_path = 'data/view_of_delft_PUBLIC/lidar/training/label_2'
-        self.train_indices_path = 'data/view_of_delft_PUBLIC/lidar/ImageSets/train.txt'
-        self.test_indices_path = 'data/view_of_delft_PUBLIC/lidar/ImageSets/test.txt'
-        self.pose_path = 'data/view_of_delft_PUBLIC/lidar/training/pose'
-        self.image_path = 'data/view_of_delft_PUBLIC/lidar/training/image_2'
-        self.calib_path = 'data/view_of_delft_PUBLIC/lidar/training/calib'
+        self._pointcloud_rel_path = 'data/view_of_delft_PUBLIC/radar/training/velodyne'
+        self._label_rel_path = 'data/view_of_delft_PUBLIC/lidar/training/label_2'
+        self._train_indices_rel_path = 'data/view_of_delft_PUBLIC/lidar/ImageSets/train.txt'
+        self._test_indices_rel_path = 'data/view_of_delft_PUBLIC/lidar/ImageSets/test.txt'
+        self._pose_rel_path = 'data/view_of_delft_PUBLIC/lidar/training/pose'
+        self._image_rel_path = 'data/view_of_delft_PUBLIC/lidar/training/image_2'
+        self._calib_rel_path = 'data/view_of_delft_PUBLIC/lidar/training/calib'
 
     @property
     def pointcloud_file_path(self):
-        return Path(self.__root_dir, self.pointcloud_file_path)
+        return self.__root_dir / self._pointcloud_rel_path
 
     @property
     def label_path(self):
-        return Path(self.__root_dir, self.label_path)
+        return self.__root_dir / self._label_rel_path
 
     @property
     def train_indices_path(self):
-        return Path(self.__root_dir, self.train_indices_path)
+        return self.__root_dir / self._train_indices_rel_path
 
     @property
     def test_indices_path(self):
-        return Path(self.__root_dir, self.test_indices_path)
+        return self.__root_dir / self._test_indices_rel_path
 
     @property
     def pose_path(self):
-        return Path(self.__root_dir, self.pose_path)
+        return self.__root_dir / self._pose_rel_path
 
     @property
     def image_path(self):
-        return Path(self.__root_dir, self.image_path)
+        return self.__root_dir / self._image_rel_path
 
     @property
     def calib_path(self):
-        return Path(self.__root_dir, self.calib_path)
+        return self.__root_dir / self._calib_rel_path
